f2a1d80b3644b7ce201397ad0f5e09f02e0f9b92
ArtemYakubovich <artem.yakubovich@swagmasha.org>
2023-05-28 12:14:51 +0300

    add Platfom break

--- Assets/Scripts/Platforms/PlatformSegment.cs
+++ Assets/Scripts/Platforms/PlatformSegment.cs
@@ -1,7 +1,17 @@
 using UnityEngine;
 
-
+[RequireComponent(typeof(Rigidbody))]
 public class PlatformSegment : MonoBehaviour
 {
+    private Rigidbody _rigidbody;
+    
+    public void Bounce(float force, Vector3 center, float radius)
+    {
+        if (TryGetComponent(out Rigidbody rigidbody))
+        {
+            rigidbody.isKinematic = false;
+            rigidbody.AddExplosionForce(force, center, radius);
+        }
+    }
     
 }
